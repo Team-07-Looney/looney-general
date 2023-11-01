4cfe4715d251c8cd85a94f8fbb3abb718e826083
Ihor Novikov <novi0003@hz.nl>
2023-11-01 14:29:10 +0100

    added simple frontend of a registration form

---
+++ frontend/src/lib/components/RegistrationForm.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<form class="grid grid-cols-1 gap-8">
+    <div>
+        <label for="name" class="text-black text-2xl font-righteous">Name</label>
+        <input id="name" name="name" class="rounded-md w-full text-gray-800 font-righteous p-1.5">
+    </div>
+    
+    <div>
+        <label for="email" class="text-black text-2xl font-righteous">Email</label>
+        <input id="email" name="email" class="rounded-md w-full text-gray-800 font-righteous p-1.5">
+    </div>
+    
+    <div>
+        <label for="password" class="text-black text-2xl font-righteous">Password</label>
+        <input id="password" type="password" name="password" class="rounded-md w-full text-gray-800 font-righteous p-1.5">
+    </div>
+
+    <button type="submit" class="py-2 px-2 bg-red-400 rounded-lg hover:bg-red-200 font-righteous">Register and get robot</button>
+</form>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,26 +1,14 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+    import RegistrationForm from "../lib/components/RegistrationForm.svelte";
+
+</script>
 
 <div class="container min-h-screen mx-auto">
 	<div class="relative bg-cover object-scale-down bg-center h-screen" style="background-image: url('/src/img/background-main.png');"></div>
 	<div class="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-16 grid grid-cols-1 gap-16">
-		<h1 class="text-center text-8xl text-black">Looney</h1>
-		<div class="bg-red-300 py-5 px-5 rounded-lg place-self-center">
-			<form class="grid grid-cols-1 gap-8">
-				<div>
-					<label for="name" class="text-black text-2xl">Name</label>
-				<input id="name" name="name" class="rounded-sm">
-				</div>
-				
-				<div>
-					<label for="email" class="text-black text-2xl">Email</label>
-				<input id="email" name="email" class="rounded-sm">
-				</div>
-				
-				<div>
-					<label for="password" class="text-black text-2xl">Password</label>
-				<input id="password" name="password" class="rounded-sm">
-				</div>
-			</form>
+		<h1 class="text-center text-8xl text-black font-righteous">Looney</h1>
+		<div class="w-full sm:max-w-md shadow-lg bg-red-300 py-5 px-5 rounded-lg place-self-center">
+			<RegistrationForm />
 		</div>
 	</div>
 </div>
