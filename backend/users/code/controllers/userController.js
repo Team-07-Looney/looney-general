8199f09ab151d29a3da6c29935e6d6ece7f64354
v-stamenova <valeria.stamenova3@gmail.com>
2023-11-20 15:13:53 +0100

    Create controller for the users

---
+++ backend/users/code/controllers/userController.js
@@ -0,0 +1,30 @@
+import { getAllUsers } from '../database/adapter.js';
+
+function getToDay() {
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  // This arrangement can be altered based on how we want the date's format to appear.
+  let currentDate = `${day}-${month}-${year}`;
+  console.log(currentDate);
+  return currentDate;
+}
+
+const tempResponse = {
+  meta: {
+    date: getToDay(),
+  },
+  data: {
+    message: 'this route is not implemented yet',
+  },
+};
+
+export async function getUsers(req, res) {
+  try {
+    tempResponse.data = await getAllUsers();
+    res.status(200).send(tempResponse);
+  } catch (err) {
+    next(err);
+  }
+}
