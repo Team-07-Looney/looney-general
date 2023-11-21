4f522997f9b404eb3f523a9ed7795eeb06795c05
v-stamenova <valeria.stamenova3@gmail.com>
2023-11-21 20:40:45 +0100

    Fetch all habits using axios

--- frontend/src/routes/habits/+page.svelte
+++ frontend/src/routes/habits/+page.svelte
@@ -1,12 +1,13 @@
 <script>
-    export let data;
-    console.log(data.habits.data);
+  // Data contains all data passed by the page server
+  export let data;
+  console.log(data);
 </script>
 
 <ul class="text-black text-3xl">
-    {#each data.habits.data as habit}
-        <li>{habit.name}</li> 
-    {/each}
+  {#each data.habits as habit}
+    <li>{habit.name}</li>
+  {/each}
 </ul>
 
 <a class="p-1.5 bg-red-400 rounded-lg" href="/habits/create">Create habit</a>
