77d3e21c9d8a0525d77b3563e2e12b61fe1810aa
v-stamenova <valeria.stamenova3@gmail.com>
2023-11-21 21:04:16 +0100

    Rework post method for creating a habit with axios

--- frontend/src/routes/habits/create/+page.server.js
+++ frontend/src/routes/habits/create/+page.server.js
@@ -1,23 +1,23 @@
+import axios from 'axios';
+
+/**
+ * createHabit is a function called when the user submits a form to create a habit
+ */
 export const actions = {
-    createHabit: async ({ cookies, request }) => {
-        const data = await request.formData();
-        console.log("New habit");
-        console.log(data);
-        console.log(JSON.stringify({
-            name: data.get("name"),
-            start_time: data.get("start_time"),
-            duration: data.get("duration")
-        }));
-        fetch(`http://localhost:3011/habits`, {
-            method: "POST",
-            body: JSON.stringify({
-                name: data.get("name"),
-                start_time: data.get("start_time"),
-                duration: parseInt(data.get("duration"))
-            })
-        }).then((res) => {
-            const response = res.json();
-            console.log(response);
-        });
-    }
+  createHabit: async ({ request }) => {
+
+    // Retrieves the data from the form
+    const formData = await request.formData();
+
+    // Set the body of the request, adds a header and sends post request to create habit
+    const data = await axios.post('http://localhost:3011/habits', {
+      name: formData.get('name'),
+      start_time: formData.get('start_time'),
+      duration: parseInt(formData.get("duration"))
+    }, {
+      headers: {
+        "Content-Type": 'application/x-www-form-urlencoded' // The header is important!
+      }
+    });
+  }
 };
