f1ed438e8e321216fc68b36359b7a68bb394e504
v-stamenova <valeria.stamenova3@gmail.com>
2023-11-24 11:53:59 +0100

    Rework the connection with the database

--- backend/users/code/database/database.js
+++ backend/users/code/database/database.js
@@ -1,27 +1,44 @@
 import sqlite3 from 'sqlite3';
 
-const DBSOURCE = "./database/db.sqlite"
+export const DBSOURCE = "./database/db.sqlite"
 
-export let db = new sqlite3.Database(DBSOURCE, (err) => {
-  if (err) {
-    console.error(err.message)
-    throw err
-  } else {
-    console.log('Connected to the SQLite database.')
+/**
+ * Establishes a connection with the user database
+ * @returns database
+ */
+export function openDatabaseConnection() {
+  const db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+      console.log(err);
+    }
 
-    db.run(`CREATE TABLE users (
+    db.run(`CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT NOT NULL, 
       email TEXT NOT NULL UNIQUE,
       password TEXT NOT NULL)`,
       (err) => {
         if (err) {
-          console.error('Table users is already created');
+          console.error('Error creating table: ', err.message);
         } else {
-          let insert = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-
-          db.run(insert, ['johnDoe', 'john.doe@foo.bar', 'looney']);
+          console.log('Table users created.');
         }
       });
-  }
-});
+  });
+
+  return db;
+}
+
+/**
+ * Closes the established relationship with the database
+ * @param {*} db The database that needs to be closed
+ */
+export function closeDatabaseConnection(db) {
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log('Closed the SQLite database connection.');
+    }
+  });
+}
