880bd6d8ffa170112d3d4cba6523373912bae173
v-stamenova <valeria.stamenova3@gmail.com>
2023-11-25 17:37:52 +0100

    Rework the database file

--- backend/users/code/database/database.js
+++ backend/users/code/database/database.js
@@ -3,27 +3,25 @@ import sqlite3 from 'sqlite3';
 export const DBSOURCE = "./database/db.sqlite"
 
 /**
- * Establishes a connection with the user database
+ * Establishes a connection with the users database
  * @returns database
  */
 export function openDatabaseConnection() {
+  // Retrieves the database
   const db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
-      console.log(err);
+      console.error(err.message);
+    } else {
+      console.log('Connected to SQLite database.');
     }
+  });
 
-    db.run(`CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      username TEXT NOT NULL, 
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL)`,
-      (err) => {
-        if (err) {
-          console.error('Error creating table: ', err.message);
-        } else {
-          console.log('Table users created.');
-        }
-      });
+  // Runs a query to check if the users table exists
+  db.get("SELECT count(*) AS tableUsersExists FROM sqlite_master WHERE type='table' AND name='users'", (err, row) => {
+    // If there is no such table create one
+    if (row.tableUsersExists == 0) {
+      createTable(db);
+    }
   });
 
   return db;
@@ -41,4 +39,23 @@ export function closeDatabaseConnection(db) {
       console.log('Closed the SQLite database connection.');
     }
   });
+}
+
+/**
+ * Creates users table
+ * @param {*} db the database in which the table should be created
+ */
+function createTable(db) {
+  db.run(`CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL, 
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL)`,
+    (err) => {
+      if (err) {
+        console.error('Error creating table: ', err.message);
+      } else {
+        console.log('Table users created.');
+      }
+    });
 }
