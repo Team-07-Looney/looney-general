1f19a71055dbc54cb160ffa8909d7877c376b9a5
Ihor Novikov <novi0003@hz.nl>
2023-11-27 14:14:42 +0100

    added async database connections

--- backend/habits/code/database/adapter.js
+++ backend/habits/code/database/adapter.js
@@ -1,11 +1,14 @@
-import { db } from './database.js';
+import { openDatabaseConnection, closeDatabaseConnection } from './database.js';
 
 export async function getAllHabitsData() {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const db = await openDatabaseConnection();
     const sql = "SELECT * FROM habits";
     const params = [];
 
     db.all(sql, params, (err, rows) => {
+      closeDatabaseConnection(db);
+
       if (err) {
         console.error(err);
         reject(err);
@@ -17,10 +20,13 @@ export async function getAllHabitsData() {
 }
 
 export async function createHabitInstance(request) {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const db = await openDatabaseConnection();
     const insert = 'INSERT INTO habits (name, start_time, duration) VALUES (?,?,?)';
 
     db.run(insert, [request.body.name, request.body.start_time, request.body.duration], (err) => {
+      closeDatabaseConnection(db);
+
       if (err) {
         console.error(err);
         reject(err);
@@ -32,10 +38,13 @@ export async function createHabitInstance(request) {
 }
 
 export async function getHabitInstanceById(id) {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const db = await openDatabaseConnection();
     const sql = `SELECT * FROM habits WHERE id='${id}'`;
 
     db.all(sql, (err, row) => {
+      closeDatabaseConnection(db);
+
       if (err) {
         console.error(err);
         reject(err);
@@ -47,10 +56,13 @@ export async function getHabitInstanceById(id) {
 }
 
 export async function editHabitInstanceById(habit, habitId) {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const db = await openDatabaseConnection();
     const update = `UPDATE habits SET name='${habit.name}', start_time='${habit.start_time}', duration='${habit.duration}' WHERE id=${habitId}`;
     
     db.run(update, (err) => {
+      closeDatabaseConnection(db);
+
       if (err) {
         console.error(err);
         reject(err);
@@ -62,10 +74,13 @@ export async function editHabitInstanceById(habit, habitId) {
 }
 
 export async function deleteHabitInstanceById(habitId) {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const db = await openDatabaseConnection();
     const query = `DELETE FROM habits WHERE id='${habitId}'`;
     
     db.run(query, (err) => {
+      closeDatabaseConnection(db);
+
       if (err) {
         console.error(err);
         reject(err);
--- backend/habits/code/database/database.js
+++ backend/habits/code/database/database.js
@@ -1,26 +1,73 @@
 import sqlite3 from 'sqlite3';
 
-const DBSOURCE = "./database/db.sqlite"
+export const DBSOURCE = "./database/db.sqlite"
 
-export let db = new sqlite3.Database(DBSOURCE, (err) => {
-  if (err) {
-    console.error(err.message)
-    throw err
-  } else {
-    console.log('Connected to the SQLite database.')
+/**
+ * Establishes a connection with the habits database
+ * @returns database
+ */
+export async function openDatabaseConnection() {
+  return new Promise(async (resolve, reject) => {
+    const db = new sqlite3.Database(DBSOURCE, (err) => {
+      if (err) {
+        console.error(err.message);
+        reject(err.message);
+      } else {
+        console.log('Connected to SQLite database.');
+      }
+
+      // Runs a query to check if the habits table exists
+      db.get("SELECT count(*) AS tableHabitsExists FROM sqlite_master WHERE type='table' AND name='habits'", async (err, row) => {
+        // If there is no such table create one
+        console.log(row);
+        if (row.tableHabitsExists == 0) {
+          await createTable(db);
+        }
+
+        resolve(db);
+      });
+    });
+  });
+}
+
+/**
+ * Closes the established relationship with the database
+ * @param {*} db The database that needs to be closed
+ */
+export function closeDatabaseConnection(db) {
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log('Closed the SQLite database connection.');
+    }
+  });
+}
+
+
+/**
+ * Creates asynchronously habits table
+ * @param {*} db the database in which the table should be created
+ */
+// The function needs to be async because otherwise other functions were taking over
+// priority (which were using the table in question) and would result in an error
+async function createTable(db) {
+  return new Promise((resolve, reject) => {
     db.run(`CREATE TABLE habits (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL, 
       start_time TEXT, 
       duration INTEGER)`,
       (err) => {
+        console.log('here? vol2');
+
         if (err) {
-          console.error('Table habits is already created');
+          console.error('Error creating table: ', err.message);
+          reject(err.message);
         } else {
-          let insert = 'INSERT INTO habits (name, start_time, duration) VALUES (?,?,?)';
-          db.run(insert, ["Brush teeth", "8:15", "30"]);
-          db.run(insert, ["Take a shower", "8:05", "10"]);
+          console.log('Table habits created.');
+          resolve();
         }
       });
-  }
-});
+  })
+}
