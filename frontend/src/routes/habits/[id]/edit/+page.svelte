e5d29d6a24e1b2d40e5a2368b22752ff05addf9c
Ihor Novikov <novi0003@hz.nl>
2023-11-27 12:34:46 +0100

    added placeholders and number inputs

--- frontend/src/routes/habits/[id]/edit/+page.svelte
+++ frontend/src/routes/habits/[id]/edit/+page.svelte
@@ -5,17 +5,17 @@
 <form method="POST" class="text-black text-2xl" action="?/editHabit">
     <div>
         <label for="name">Name</label>
-        <input id="name" name="name" class="rounded-sm" value={data.habit[0].name}>
+        <input id="name" placeholder="e.g. Brush teeth" name="name" class="rounded-sm" value={data.habit[0].name}>
     </div>
 
     <div>
         <label for="start_time">Start time</label>
-        <input id="start_time" name="start_time" class="rounded-sm" value={data.habit[0].start_time}>
+        <input id="start_time" placeholder="e.g. 10:00" name="start_time" class="rounded-sm" value={data.habit[0].start_time}>
     </div>
 
     <div>
         <label for="duration">Duration</label>
-        <input id="duration" name="duration" class="rounded-sm" value={data.habit[0].duration}>
+        <input type="number" min="0" placeholder="e.g. 120" id="duration" name="duration" class="rounded-sm" value={data.habit[0].duration}>
     </div>
 
     <button class="p-1.5 bg-red-400 rounded-lg mt-3" type="submit">Submit</button>
--- frontend/src/routes/habits/create/+page.svelte
+++ frontend/src/routes/habits/create/+page.svelte
@@ -1,17 +1,17 @@
 <form method="POST" class="text-black text-2xl" action="?/createHabit">
     <div>
         <label for="name">Name</label>
-        <input id="name" name="name" class="rounded-sm">
+        <input id="name" placeholder="e.g. Brush teeth" name="name" class="rounded-sm">
     </div>
 
     <div>
         <label for="start_time">Start time</label>
-        <input id="start_time" name="start_time" class="rounded-sm">
+        <input id="start_time" placeholder="e.g. 10:00" name="start_time" class="rounded-sm">
     </div>
 
     <div>
-        <label for="duration">Duration</label>
-        <input id="duration" name="duration" class="rounded-sm">
+        <label for="duration">Duration (seconds)</label>
+        <input type="number" placeholder="e.g. 120" min="0" id="duration" name="duration" class="rounded-sm">
     </div>
 
     <button class="p-1.5 bg-red-400 rounded-lg mt-3" type="submit">Create</button>
