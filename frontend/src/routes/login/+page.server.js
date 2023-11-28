9781bd0dc282befd67737e4af18c5234fa0e4851
v-stamenova <valeria.stamenova3@gmail.com>
2023-11-28 09:23:40 +0100

    Create user friendly login errors

--- frontend/src/routes/login/+page.server.js
+++ frontend/src/routes/login/+page.server.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { redirect } from '@sveltejs/kit';
+import { fail } from '@sveltejs/kit';
 
 export const actions = {
   login: async ({ cookies, request }) => {
@@ -7,21 +8,59 @@ export const actions = {
 
     const formData = await request.formData();
 
-    const data = await axios.post('http://localhost:3011/login', {
-      password: formData.get('password'),
-      email: formData.get('email'),
-    }, {
-      headers: {
-        "Content-Type": 'application/x-www-form-urlencoded' // The header is important!
+    let password = formData.get('password')
+    let email = formData.get('email');
+
+    let errors = await validateLoginData(formData);
+    if (errors.length > 0) {
+      return fail(400, { email, errors });
+    }
+
+    try {
+      const data = await axios.post('http://localhost:3011/login', {
+        password: password,
+        email: email,
+      }, {
+        headers: {
+          "Content-Type": 'application/x-www-form-urlencoded' // The header is important!
+        }
+      });
+
+      cookies.set('jwt', data.data.data.token, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 // 1 day
+      });
+    } catch (err) {
+      if (err.response.data.data.message == 'Issue with the credentials') {
+        errors = [{ "input": "email", "message": "There seems to be an issue with the credentials" },
+        { "input": "password", "message": "" }];
       }
-    });
 
-    cookies.set('jwt', data.data.data.token, {
-      httpOnly: true,
-      maxAge: 60 * 60 * 24 // 1 day
-    });
+      return fail(400, { email, errors });
+    }
 
-    //console.log(data.data.data.token);
     throw redirect(302, '/habits');
   }
 };
+
+async function validateLoginData(formData) {
+  let email = formData.get('email');
+  let password = formData.get('password');
+
+  let validationIssues = [];
+
+  if (!email) {
+    validationIssues.push({ "input": "email", "message": "The email is missing" });
+  } else {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!emailRegex.test(formData.get('email'))) {
+      validationIssues.push({ "input": "email", "message": "The email that has been provided is invalid" });
+    }
+  }
+
+  if (!(password)) {
+    validationIssues.push({ "input": "password", "message": "The password is missing" });
+  }
+
+  return validationIssues;
+}
