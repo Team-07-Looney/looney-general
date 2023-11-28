d5b2a17ed091f8fc1eda78d3ac125ad6fcc92e6e
root <root@LAPTOP-P2QTQKJP>
2023-11-28 16:28:33 +0100

    One way of scrolling implemented

--- frontend/src/routes/habits/+page.svelte
+++ frontend/src/routes/habits/+page.svelte
@@ -6,20 +6,18 @@
 </script>
 
 
-  <h1 class="text-center text-8xl text-black font-chewy">Habits</h1>
+<a href="/main" type="button" class="p-1.5"><img src="../src/img/backButton.png" style="height: 50px;"></a>
+<a href="/" type="button" class="p-2" style="float: right;"><img src="../src/img/menuButton.png" class="h-10"></a>
+<h1 class="text-center text-8xl text-black font-chewy">Habits</h1>
 
-  <ul>
-    {#each data.habits as habit}
-      <li class="px-10 py-2"><a href={`habits/${habit.id}`}>
-        <Habit title={habit.name} time={habit.start_time} iconCount={habit.id}/></a>
-      </li> 
-    {/each}
-  </ul>
+<ul style="overflow-y: scroll; height:300px;">
+  {#each data.habits as habit}
+    <li class="px-10 py-2"><a href={`habits/${habit.id}`}>
+      <Habit title={habit.name} time={habit.start_time} iconCount={habit.id%4}/></a>
+    </li> 
+  {/each}
+</ul>
 
-<a href="/" type="button" class="p-1.5 bg-red-400 rounded-lg">
-    Home 
-</a>
-<a href="/" type="button" class="p-1.5 bg-red-400 rounded-lg">
-    Back button 
-</a>
-<a class="p-1.5 bg-red-400 rounded-lg" href="/habits/create">Create habit</a>
+<div class="flex justify-center items-center">
+<a class="p-2" href="/habits/create"><img src="../src/img/addButton.png" style="height: 50px;" class="h-10"></a>
+</div>
