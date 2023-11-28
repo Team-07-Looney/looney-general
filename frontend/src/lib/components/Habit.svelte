257cdcb212b120fd4ab8e1614f53ca59a03b7657
root <root@LAPTOP-P2QTQKJP>
2023-11-28 15:36:11 +0100

    Initial design of the list of habits implemented, missing buttons and icon logic

---
+++ frontend/src/lib/components/Habit.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let title;
+    export let time;
+    export let iconCount;
+</script>
+<div class="bg-white rounded-xl shadow-2xl flex p-1">
+    <div>
+        <img src="../../src/img/icon{iconCount}.png" style="height: 50px;" class="p-2">
+    </div>
+    <div>
+        <h1 class="text-lg"><strong>{title}</strong></h1>
+        <p>Start time {time}</p>
+    </div>
+    <div style="float: left;">
+        <a href="">...</a>
+    </div>
+</div>
--- frontend/src/routes/habits/+page.svelte
+++ frontend/src/routes/habits/+page.svelte
@@ -1,17 +1,21 @@
 <script>
-  // Data contains all data passed by the page server
-  export let data;
+    import Habit from "../../lib/components/Habit.svelte";
+
+    // Data contains all data passed by the page server
+    export let data;
 </script>
 
 
   <h1 class="text-center text-8xl text-black font-chewy">Habits</h1>
 
+  <ul>
+    {#each data.habits as habit}
+      <li class="px-10 py-2"><a href={`habits/${habit.id}`}>
+        <Habit title={habit.name} time={habit.start_time} iconCount={habit.id}/></a>
+      </li> 
+    {/each}
+  </ul>
 
-<ul class="text-black text-3xl">
-  {#each data.habits as habit}
-    <li class="p-1 bg-red-400 rounded-lg mt-2" ><a href={`habits/${habit.id}`}>{habit.name}</a></li>
-  {/each}
-</ul>
 <a href="/" type="button" class="p-1.5 bg-red-400 rounded-lg">
     Home 
 </a>
