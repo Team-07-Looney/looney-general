1b19090f57bcb5dcc3b36c38d325c9e855dbd618
anafaraco01 <rive0008@hz.nl>
2023-12-05 20:56:05 +0100

    Timer style improved

--- frontend/src/lib/components/Timer.svelte
+++ frontend/src/lib/components/Timer.svelte
@@ -80,58 +80,58 @@
  
 <div class="flex flex-col justify-center items-center mt-4">
   <div class="flex flex-row">
-  <svg viewBox="-50 -50 100 100" width="150" height="150">
-    <title>{count}</title>
-    <g fill="none" stroke="currentColor" stroke-width="8">
-      <circle stroke="currentColor" r="46" />
-      <path
-        stroke="hsl(208, 100%, 50%)"
-        d="M 0 -46 a 46 46 0 0 0 0 92 46 46 0 0 0 0 -92"
-        pathLength="1"
-        stroke-dasharray="1"
-        stroke-dashoffset={isStarted ? $offset : '0'}
-      />
-    </g>
-    <g fill="hsl(208, 100%, 50%)" stroke="none">
-      <g transform="rotate({isStarted ? $rotation : '0'})">
-        <g transform="translate(0 -46)">
-          <circle r="4" />
+    <svg viewBox="-60 -60 120 120" width="180" height="180">
+      <title>{count}</title>
+      <g fill="none" stroke="gray" stroke-width="9">
+        <circle stroke="grayr" r="46" />
+        <path
+          stroke="purple"
+          d="M 0 -46 a 46 46 0 0 0 0 92 46 46 0 0 0 0 -92"
+          pathLength="1"
+          stroke-dasharray="1"
+          stroke-dashoffset={isStarted ? $offset : '0'}
+        />
+      </g>
+      <g fill="purple" stroke="none">
+        <g transform="rotate({isStarted ? $rotation : '0'})">
+          <g transform="translate(0 -46)">
+            <circle r="4" />
+          </g>
         </g>
       </g>
-    </g>
- 
-    <g
-      fill="currentColor"
-      text-anchor="middle"
-      dominant-baseline="baseline"
-      font-size="20"
-    >
-      <text x="-3" y="6.5">
-        {#each Object.entries({ h, m, s }) as [key, value], i}
-          {#if countdown >= 60 ** (2 - i)}
-            <tspan dx="3" font-weight="bold">{padValue(value)}</tspan><tspan
-              dx="0.5"
-              font-size="10">{key}</tspan
-            >
-          {/if}
-        {/each}
-      </text>
-    </g>
-  </svg>
+  
+      <g
+        fill="black"
+        text-anchor="middle"
+        dominant-baseline="baseline"
+        font-size="20"
+      >
+        <text x="-3" y="6.5">
+          {#each Object.entries({ h, m, s }) as [key, value], i}
+            {#if countdown >= 60 ** (2 - i)}
+              <tspan dx="3" font-weight="bold">{padValue(value)}</tspan><tspan
+                dx="0.5"
+                font-size="10">{key}</tspan
+              >
+            {/if}
+          {/each}
+        </text>
+      </g>
+    </svg>
   </div>
 
   <!-- Buttons need to be refactor code duplicatio with the stop -->
   <div class="flex flex-row justify-center items-center mb-4 mt-6">
     {#if !isStarted}
-      <button on:click={handleStart} class="bg-blue-500 rounded-lg p-2 text-white min-w-[85px]">Start</button>  
+      <button on:click={handleStart} class="bg-[#383e4d] rounded-lg p-2 text-white min-w-[85px]">Start</button>  
     {:else if isOver}
       <button on:click={handleReset} class="bg-gray-300 rounded-lg p-2 text-black min-w-[85px]">Reset</button>
     {:else if !isPaused}
       <button on:click={handlePause} class="bg-gray-300 rounded-lg p-2 text-black min-w-[85px] mr-2">Pause</button>
-      <button class="bg-blue-500 rounded-lg p-2 text-white min-w-[85px] ml-2"><a href="/habits">Stop</a></button> 
+      <button class="bg-[#383e4d] rounded-lg p-2 text-white min-w-[85px] ml-2"><a href="/habits">Done</a></button> 
     {:else}
       <button on:click={handleStart} class="bg-gray-300 rounded-lg p-2 text-black min-w-[85px] mr-2">Resume</button>
-      <button class="bg-blue-500 rounded-lg p-2 text-white min-w-[85px] ml-2"><a href="/habits">Stop</a></button>   
+      <button class="bg-[#383e4d] rounded-lg p-2 text-white min-w-[85px] ml-2"><a href="/habits">Done</a></button>   
     {/if}
   </div>
 </div>
