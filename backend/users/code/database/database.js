da8d65d3b950cd745c70ae0669060ea8b4146184
v-stamenova <valeria.stamenova3@gmail.com>
2023-12-11 13:49:16 +0100

    Add in-memory database when in testing env

--- backend/users/code/database/database.js
+++ backend/users/code/database/database.js
@@ -1,6 +1,14 @@
 import sqlite3 from 'sqlite3';
 
-export const DBSOURCE = "./database/db.sqlite"
+export let DBSOURCE = "./database/db.sqlite"
+
+/**
+ * When running unit tests with npm test it uses the memory database instead of the 
+ * sqlite file ./database/db.sqlite
+ */
+if (process.env.NODE_ENV === 'test') {
+  DBSOURCE = ":memory:";
+}
 
 /**
  * Establishes a connection with the users database
@@ -46,7 +54,7 @@ export function closeDatabaseConnection(db) {
  */
 // The function needs to be async because otherwise other functions were taking over
 // priority (which were using the table in question) and would result in an error
-async function createTable(db) {
+export async function createTable(db) {
   return new Promise((resolve, reject) => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -56,6 +64,7 @@ async function createTable(db) {
       (err) => {
         if (err) {
           console.error('Error creating table: ', err.message);
+          console.log('Error creating table: ', err.message);
           reject(err.message);
         } else {
           console.log('Table users created.');
