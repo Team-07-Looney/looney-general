64f89b95c316a95e8f198e7f6487dd49635cc3cd
LauraBirau <bira0001@hz.nl>
2023-12-14 11:29:48 +0100

    added selection of mood and reason

--- frontend/src/lib/components/MoodItem.svelte
+++ frontend/src/lib/components/MoodItem.svelte
@@ -3,10 +3,11 @@
     export let moodId;
 </script>
 
-<div class="bg-white rounded-xl p-1 flex justify-between" style="box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px">
-    <a href={`moods/${moodId}`} class="flex h-full">
-        <div>
-            <h1 class="text-lg"><strong>{title}</strong></h1>
-        </div>
-    </a>
+<div
+    class="hover:bg-neutral-400 active:bg-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300 bg-neutral-100 rounded-xl p-1 flex justify-between"
+    style="box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px"
+>
+    <div>
+        <h1 class="text-lg"><strong>{title}</strong></h1>
+    </div>
 </div>
--- frontend/src/routes/moods/my-mood/+page.svelte
+++ frontend/src/routes/moods/my-mood/+page.svelte
@@ -1,10 +1,12 @@
 <script>
   import Header from "../../../lib/components/Header.svelte";
   import MoodItem from "../../../lib/components/MoodItem.svelte";
-
+  let moods = document.getElementsByTagName("li");
+  // moods.array.forEach(element => {
+  //   console.log(element);
+  // });
   // Data contains all data passed by the page server
   export let data;
-  console.log(data);
 </script>
 
 <Header title="My mood" displayBackButton="1" displayMenu="1" route="/moods" />
@@ -17,8 +19,8 @@
   <div class="flex flex-row">
     <ul>
       {#each data.moods as mood}
-        <li class="px-10 py-2">
-          <MoodItem title={mood.name} time={mood.start_time} moodId={mood.id} />
+        <li class="px-10 py-2" id="element">
+          <MoodItem title={mood.name} moodId={mood.id} />
         </li>
       {/each}
     </ul>
@@ -29,18 +31,20 @@
   <div class="flex flex-row">
     <ul>
       {#each data.reasons as reason}
-        <li class="px-10 py-2">
-          <MoodItem title={reason.name} moodId={reason.id} />
-        </li>
+        <div class="px-10 py-2">
+          <MoodItem title={reason.name} moodId={reason.id}/>
+        </div>
       {/each}
     </ul>
   </div>
-  <div class="flex flex-row">
-    <button class="bg-neutral-400 text-white font-bold p-2 rounded-lg w-20"
-      >Cancel</button
+  <div class="flex flex-row gap-8">
+    <a href="/moods" class="bg-neutral-400 text-white font-bold p-2 rounded-lg w-20"
+    style="z-index: 10;"
+      >Cancel
+      </a
     >
-    <button class="bg-indigo-300 text-white font-bold p-2 rounded-lg w-20"
-      >Next</button
-    >
+    <div class="flex justify-center items-center relative" style="z-index: 10;">
+    <a class="bg-indigo-300 text-white font-bold p-2 rounded-lg w-20" href="/moods/my-mood/thought">Next</a>
+    </div>
   </div>
 </div>
