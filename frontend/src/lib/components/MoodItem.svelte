a430a7ab3c7a926c4ae195512b2616f6d4caa432
anafaraco01 <rive0008@hz.nl>
2023-12-16 18:53:05 +0100

    Formatted document

--- frontend/src/lib/components/MoodItem.svelte
+++ frontend/src/lib/components/MoodItem.svelte
@@ -7,30 +7,30 @@
 
   const dispatch = createEventDispatcher();
 
-
   function handleMoodSelection(id) {
     dispatch("moodSelected", {
       moodId: moodId,
     });
 
-    let moodElement = document.getElementById(`moodElement${id}`);  
-  if(storedMoodId == 0){
-moodElement.classList.add( 'bg-gray-400');
-storedMoodId = id;
-  }
-  else {
-    let previousMoodElement = document.getElementById(`moodElement${storedMoodId}`);
-    previousMoodElement.classList.remove('bg-gray-400');
-    moodElement.classList.add( 'bg-gray-400');
-    storedMoodId = id;
-  }
+    let moodElement = document.getElementById(`moodElement${id}`);
+    if (storedMoodId == 0) {
+      moodElement.classList.add("bg-gray-400");
+      storedMoodId = id;
+    } else {
+      let previousMoodElement = document.getElementById(
+        `moodElement${storedMoodId}`,
+      );
+      previousMoodElement.classList.remove("bg-gray-400");
+      moodElement.classList.add("bg-gray-400");
+      storedMoodId = id;
+    }
   }
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
-id="moodElement{moodId}"
+  id="moodElement{moodId}"
   class="font-light active:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-300 bg-gray-100 rounded-lg py-1 px-3"
   on:click={handleMoodSelection(moodId)}
 >
--- frontend/src/routes/moods/my-mood/+page.svelte
+++ frontend/src/routes/moods/my-mood/+page.svelte
@@ -19,68 +19,65 @@
     console.log("Reason clicked:", event.detail.reasonId);
     reasonId = event.detail.reasonId;
   }
- 
-  let storedMoodId = 0;
- 
 
+  let storedMoodId = 0;
 </script>
+
 <WhiteBanner
-title="My Mood"
-description="Gain more insight in how you are feeling, Looney will always be there
+  title="My Mood"
+  description="Gain more insight in how you are feeling, Looney will always be there
 "
-route="/moods"
-imgExtraPath="../"
+  route="/moods"
+  imgExtraPath="../"
 />
 
 <div
   class="bg-white z-20 mt-1 p-3 mr-6 ml-6 rounded-lg min-h-fit flex flex-col justify-center items-center"
 >
-<form method="POST" action="?/createRecord">
-
-  <div class="flex justify-center items-center">
-    <h2 class="text-xl">How do you feel?</h2>
-  </div>
-  <div class="flex flex-wrap justify-center items-center mt-2 mb-2">
-    {#each data.moods as mood}
-      <div class="p-1" id="element">
-        <MoodItem
-          title={mood.name}
-          moodId={mood.id}
-          bind:storedMoodId={storedMoodId}
-          on:moodSelected={handleMoodClick}
-        />
-      </div>
-    {/each}
-  </div>
-
-  <div class="flex justify-center items-center">
-    <h2 class="text-xl">Why do you feel this way?</h2>
-  </div>
-  <div class="flex flex-wrap justify-center items-center mt-2">
-      {#each data.reasons as reason}
+  <form method="POST" action="?/createRecord">
+    <div class="flex justify-center items-center">
+      <h2 class="text-xl">How do you feel?</h2>
+    </div>
+    <div class="flex flex-wrap justify-center items-center mt-2 mb-2">
+      {#each data.moods as mood}
         <div class="p-1" id="element">
-        <ReasonItem
-          title={reason.name}
-          reasonId={reason.id}
-          on:reasonSelected={handleReasonClick}
-        />
+          <MoodItem
+            title={mood.name}
+            moodId={mood.id}
+            bind:storedMoodId
+            on:moodSelected={handleMoodClick}
+          />
         </div>
       {/each}
-  </div>
+    </div>
 
+    <div class="flex justify-center items-center">
+      <h2 class="text-xl">Why do you feel this way?</h2>
+    </div>
+    <div class="flex flex-wrap justify-center items-center mt-2">
+      {#each data.reasons as reason}
+        <div class="p-1" id="element">
+          <ReasonItem
+            title={reason.name}
+            reasonId={reason.id}
+            on:reasonSelected={handleReasonClick}
+          />
+        </div>
+      {/each}
+    </div>
 
-  <input type="hidden" name="moodId" bind:value={moodId} />
-  <input type="hidden" name="reasonId"  bind:value={reasonId} />
-  <div class="flex flex-row gap-8 justify-center items-center mt-2">
-    <a
-      class="z-10 flex justify-center items-center bg-neutral-400 text-white font-bold p-2 rounded-lg w-20"
-      href="/moods"
-    >Cancel</a>
-    <button
-      class="z-10 bg-indigo-300 text-white font-bold p-2 rounded-lg w-20"
-      type="submit"
-    >Next</button>
-  </div>
-</form>
+    <input type="hidden" name="moodId" bind:value={moodId} />
+    <input type="hidden" name="reasonId" bind:value={reasonId} />
+    <div class="flex flex-row gap-8 justify-center items-center mt-2">
+      <a
+        class="z-10 flex justify-center items-center bg-neutral-400 text-white font-bold p-2 rounded-lg w-20"
+        href="/moods">Cancel</a
+      >
+      <button
+        class="z-10 bg-indigo-300 text-white font-bold p-2 rounded-lg w-20"
+        type="submit">Next</button
+      >
+    </div>
+  </form>
 </div>
 <BottomMenu imgPath="../../../" displayMoodText="1" />
