fd176cc38cb49d3dd81459be508d0d4baabcfb62
Jessica Dinova <dino0001@hz.nl>
2024-01-08 10:35:25 +0100

    pull request comments resolved

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div>
-	<WhiteBanner title="Looney" description="Please Login or Register" displayMenuButton=0 imgExtraPath=""/>
+	<WhiteBanner title="Looney" description="Please Login or Register" displayMenuButton=0/>
 	<div class=" mt-32 flex flex-col justify-center items-center text-center">
 		<div>
 			<a href="/login" type="button" class="bg-white rounded-lg shadow-lg text-lg min-w-[200px] p-2 flex-row m-2">
