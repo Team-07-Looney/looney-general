f4dce753bd40a0bcdcc6a736636ea8cfd7878e57
Laura <bira0001@hz.nl>
2024-01-09 15:49:25 +0100

    selected mood from record added

--- frontend/src/routes/moods/my-mood/[recordId]/thought/advice/+page.server.js
+++ frontend/src/routes/moods/my-mood/[recordId]/thought/advice/+page.server.js
@@ -34,54 +34,84 @@ export const load = async ({ serverLoadEvent, cookies, params }) => {
       }
     });
 
-    if (isAuthenticated.data.message == "User is authenticated") {
-        isUserAuth = true;
+
+    const adviceResponse = await axios.get('http://localhost:3011/advice', {
+      headers: {
+        'Authorization': `Bearer ${jwtoken}`
       }
-      const records = recordsResponse.data.data;
-      const userId = payload.id;
-      const moods = moodsResponse.data.data;
+    });
 
-      const filteredRecordsByUser = records.filter(record => record.user_id === userId);
-      const moodTypeCounts = {};
-      let totalMoodTypes = 0;
-
-      // Iterate over filteredRecordsByUser
-      filteredRecordsByUser.forEach((record) => {
-        // Find the corresponding mood in the moodTable
-        const mood = moods.find((m) => m.id === parseInt(record.mood_id.split('/').pop(), 10));
-        totalMoodTypes += 1;
-        // Check if mood is found
-        if (mood) {
-            const { mood_type_id } = mood;
-            // Increment the count for the mood_type_id
-            moodTypeCounts[mood_type_id] = (moodTypeCounts[mood_type_id] || 0) + 1;
-        }
-      });
-    
-        const moodType1 = '/mood-types/1'; 
-        const moodType2 = '/mood-types/2'; 
-        const moodType3 = '/mood-types/3';
-        const positiveTotal = moodTypeCounts[moodType1] || 0;
-        const neutralTotal = moodTypeCounts[moodType2] || 0;
-        const negativeTotal = moodTypeCounts[moodType3] || 0;
-
-        const statistics = {
-            "positive": positiveTotal,
-            "neutral": neutralTotal,
-            "negative": negativeTotal,
-            "total": totalMoodTypes
-        };
-
-      return { statistics, recordId};
-    } catch (error) {
-      console.log(error);
+    if (isAuthenticated.data.message == "User is authenticated") {
+      isUserAuth = true;
     }
+    const records = recordsResponse.data.data;
+    const userId = payload.id;
+    const moods = moodsResponse.data.data;
+    const advice = adviceResponse.data.data;
+    const filteredRecordsByUser = records.filter(record => record.user_id === userId);
+    const moodTypeCounts = {};
+    let totalMoodTypes = 0;
+
+    // Iterate over filteredRecordsByUser
+    filteredRecordsByUser.forEach((record) => {
+      // Find the corresponding mood in the moodTable
+      const mood = moods.find((m) => m.id === parseInt(record.mood_id.split('/').pop(), 10));
+      totalMoodTypes += 1;
+      // Check if mood is found
+      if (mood) {
+        const { mood_type_id } = mood;
+        // Increment the count for the mood_type_id
+        moodTypeCounts[mood_type_id] = (moodTypeCounts[mood_type_id] || 0) + 1;
+      }
+    });
+
+    const moodType1 = '/mood-types/1';
+    const moodType2 = '/mood-types/2';
+    const moodType3 = '/mood-types/3';
+    const positiveTotal = moodTypeCounts[moodType1] || 0;
+    const neutralTotal = moodTypeCounts[moodType2] || 0;
+    const negativeTotal = moodTypeCounts[moodType3] || 0;
+
+    const statistics = {
+      "positive": positiveTotal,
+      "neutral": neutralTotal,
+      "negative": negativeTotal,
+      "total": totalMoodTypes
+    };
+
+    const selectedRecord = filteredRecordsByUser.find((record) => record.id === parseInt(recordId));
+    const selectedMood = moods.find((mood) => mood.id === parseInt(selectedRecord.mood_id.split('/').pop(), 10));
+    console.log(selectedMood);
   
-  
-    // If they are authenticated they should not be able to access the login
-    // Svelte issue with throwing redirect within a try-catch block workaround
-    if (!isUserAuth) {
-      throw redirect(302, "/login");
-    }
-  };
-  
+    // const filteredAdviceBySelectedMoodType = advice.filter(advice => advice.mood_type_id === selectedMoodType);
+    // // Function to pick two random pieces of advice
+    // function pickTwoRandomAdvice(array) {
+    //   // Generate random pieces of advice
+    //   const advice1 = Math.floor(Math.random() * array.length);
+    //   let advice2 = Math.floor(Math.random() * array.length);
+
+    //   // Make sure the second advice is different from the first
+    //   while (advice2 === advice1) {
+    //     advice2 = Math.floor(Math.random() * array.length);
+    //   }
+
+    //   // Return the two random pieces of advice
+    //   return [array[advice1], array[advice2]];
+    // }
+
+    // // Call the function and get two random instances
+    // const randomInstances = pickTwoRandomAdvice(filteredAdviceBySelectedMoodType);
+    // // console.log(randomInstances);
+    return { statistics, recordId };
+  } catch (error) {
+    console.log(error);
+  }
+
+
+  // If they are authenticated they should not be able to access the login
+  // Svelte issue with throwing redirect within a try-catch block workaround
+  if (!isUserAuth) {
+    throw redirect(302, "/login");
+  }
+};
+
