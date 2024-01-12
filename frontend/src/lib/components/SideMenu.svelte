c0e87281f5c0e695abbf2656478ded09ca973082
v-stamenova <valeria.stamenova3@gmail.com>
2024-01-12 08:36:06 +0100

    Move the logout from `profile` to specific route

--- frontend/src/lib/components/SideMenu.svelte
+++ frontend/src/lib/components/SideMenu.svelte
@@ -28,7 +28,7 @@
     >
       <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
     </svg>
-    <a href="/" type="button" class="p-1.5 w-full"><p class=" text-2xl">Logout</p></a>
+    <a href="/logout" type="button" class="p-1.5 w-full"><p class=" text-2xl">Logout</p></a>
     </div>
 </div>
 
