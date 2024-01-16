5812397be2e478c2a707ad5c6eea2003b4d7b17a
Jessica Dinova <dino0001@hz.nl>
2024-01-16 15:52:39 +0100

    fixed eslint errors + user cannot access the page when they are logged in

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -11,11 +11,20 @@
     displayMenuButton="0"
     displayBackButton="0"
   />
-  <div class="h-full flex flex-col justify-center items-center text-center text-xl pt-[10.5rem]">
+  <div
+    class="h-full flex flex-col justify-center items-center text-center text-xl pt-[10.5rem]"
+  >
     <div class="w-full flex justify-center items-center absolute z-10 top-40">
-      <img src="/src/img/mainLooney.png" alt="Looney" class="h-52 drop-shadow-lg">
+      <img
+        src="/src/img/mainLooney.png"
+        alt="Looney"
+        class="h-52 drop-shadow-lg"
+      />
     </div>
-    <div class="bg-white pt-16 pb-6 px-5 rounded-lg" style="box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px">
+    <div
+      class="bg-white pt-16 pb-6 px-5 rounded-lg"
+      style="box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px"
+    >
       <p>Let's get started</p>
       <div>
         <a
@@ -39,5 +48,5 @@
         </a>
       </div>
     </div>
-    </div>
   </div>
+</div>
